279a4151ebb08c868ade4d29e443443dee43356f
Yihai Duan <yihaiduan@126.com>
2019-03-06 00:17:43 +0800

    ui

--- project/js/ship.js
+++ project/js/ship.js
@@ -1,14 +1,14 @@
 window.onload = function() {
 	var ship_num = 5;
+	var row = 10;
 	var rand = Math.floor(Math.random()*10);
+	var arr = [18,18,6,8,10];
 	function allowDrop(ev){
 		ev.preventDefault();
+		//console.log(ev);
 	}
 	function drag(ev){
-		//alert(1);
-
 		ev.dataTransfer.setData("Text",ev.target.id);
-		
 	}
 	function drop(ev){
 		ev.preventDefault();
@@ -23,14 +23,36 @@ window.onload = function() {
 		ev.target.ondrop = null;
 		ev.target.ondragover = null;
 	}
-	
-	for (var i = 0; i < ship_num; i++) {
-		var rand = Math.floor(Math.random()*(100-i));
-		var tds_empty = document.querySelectorAll(".empty");
+	function isEmpty(start,width, tds){
+		console.log(1);
+		for(var i=0; i<width; i++){
+			if (tds[start+i].className == "battle_cell hasShip")
+				return false;
+		}
+		return true;
+	}
+	var tds_empty = document.querySelectorAll(".empty");
+	for (var i = 0; i < 5; i++) {
+		var width = arr[i]/2;
+/*		var rand = Math.floor(Math.random()*(100));
+		var remain = rand%row;
+		
+		var newRemain = remain + width > row ?row-width: remain;
+		rand = rand - remain + newRemain;*/
+		do {
+			//alert(rand);
+			var rand = Math.floor(Math.random()*(100));
+			var remain = rand%row;s
+			var newRemain = remain + width > row ? row-width: remain;
+			rand = rand - remain + newRemain;
+		}while(!isEmpty(rand,width,tds_empty));
 		var div = document.createElement("div");
-		div.className = "ship_draggable ship_r";
+		div.className = "ship_draggable ship_r"; 
+
+		div.style.width = arr[i] + "em";
 		tds_empty[rand].childNodes[0].appendChild(div);
-		tds_empty[rand].className = "battle_cell hasShip";
+		for (var j=0; j<width; j++)
+			tds_empty[rand+j].className = "battle_cell hasShip";
 	}
 
 	var ships = document.querySelectorAll(".ship_draggable");
